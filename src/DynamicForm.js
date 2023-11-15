a0519f6dc93b7d6a0c58e4c3ca42f78fc5a665a9
Kyle Montgomery <kyle.montgomery@safehealth.me>
2023-11-15 09:49:35 -0800

    deploy new form

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -4,21 +4,37 @@ import formStructure from './formStructure.json'; // Adjust the path if needed
 const DynamicForm = () => {
     const [formData, setFormData] = useState({});
 
-    const handleChange = (event, parentPath) => {
-        const { name, value } = event.target;
-        const fullPath = parentPath ? `${parentPath}.${name}` : name;
-        const nestedKeys = fullPath.split('.');
-        const lastKey = nestedKeys.pop();
-        const lastObj = nestedKeys.reduce((obj, key) => {
-            if (key.endsWith(']')) {
-                // Handling array index
-                const match = key.match(/(.+)\[(\d+)\]/);
-                return (match && obj[match[1]] && obj[match[1]][match[2]]) || {};
-            }
-            return (obj[key] = obj[key] || {});
-        }, formData);
-        lastObj[lastKey] = value;
-        setFormData({ ...formData });
+    const handleChange = (event, path) => {
+        const { value } = event.target;
+        setFormData(prevState => {
+            const newState = { ...prevState };
+            const keys = path.split('.');
+            keys.reduce((current, key, index) => {
+                if (index === keys.length - 1) {
+                    current[key] = value;
+                } else {
+                    current[key] = current[key] || {};
+                }
+                return current[key];
+            }, newState);
+            return newState;
+        });
+    };    
+
+    const renderDropdown = (path, options) => {
+        return (
+            <select 
+                name={path} 
+                value={formData[path] || ''} 
+                onChange={(e) => handleChange(e, path)}
+            >
+                {options.map((option, index) => (
+                    <option key={`${path}-${index}`} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        );
     };
 
     const renderFormFields = (data, parentPath = '') => {
@@ -31,41 +47,21 @@ const DynamicForm = () => {
                         <label>{key.replace(/\.\d+/g, '')}:</label>
                         <input 
                             type="text" 
-                            name={key} 
+                            name={path} 
                             value={formData[path] || ''} 
-                            onChange={(e) => handleChange(e, parentPath)} 
+                            onChange={(e) => handleChange(e, path)} 
                         />
                     </div>
                 );
-            } else if (Array.isArray(value)) {
+            } else if (Array.isArray(value) && value.every(item => typeof item !== 'object')) {
+                // Render dropdown if the array doesn't contain objects
                 return (
                     <div key={path}>
                         <label>{key}:</label>
-                        {value.map((item, index) => {
-                            const itemPath = `${path}[${index}]`;
-                            if (typeof item === 'object') {
-                                return (
-                                    <div key={itemPath}>
-                                        {renderFormFields(item, itemPath)}
-                                    </div>
-                                );
-                            } else {
-                                return (
-                                    <div key={itemPath}>
-                                        <label>{item}:</label>
-                                        <input 
-                                            type="text" 
-                                            name={itemPath} 
-                                            value={formData[itemPath] || ''} 
-                                            onChange={(e) => handleChange(e, itemPath)} 
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
+                        {renderDropdown(path, value)}
                     </div>
                 );
-            } else if (typeof value === 'object') {
+            } else if (Array.isArray(value) || typeof value === 'object') {
                 return (
                     <fieldset key={path}>
                         <legend>{key}</legend>
